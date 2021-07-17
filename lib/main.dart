e90b94e8f934a5aa4cc56f562338c6d5cb573d0b
Ravi <ravichandran052015@gmail.com>
2021-07-17 17:02:43 +0530

    Creating a button

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main () => runApp (MaterialApp(
+  home:Hello()
+));
+
+  class Hello extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text ('New start01', style: TextStyle(color:Colors.black,fontSize: 20),),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child:FlatButton(
+            onPressed: (){},
+          child: Text('Hello Everyone',style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
+          color: Colors.amber,
+        ),
+      ),
+      );
+    }
+  }
